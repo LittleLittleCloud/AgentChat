68a0e94b157b91782c2a205f1ada8cff574ed923
XiaoYun Zhang <xiaoyuz@microsoft.com>
2023-11-14 20:50:53 -0800

    support top-level statement function call

---
+++ src/AgentChat.SourceGenerator.Tests/TopLevelStatementFunctionExample.cs
@@ -0,0 +1,15 @@
+﻿using AgentChat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public partial class TopLevelStatementFunctionExample
+{
+    [FunctionAttribution]
+    public Task<string> Add(int a, int b)
+    {
+        return Task.FromResult($"{a + b}");
+    }
+}
